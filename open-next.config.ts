e655e6106e1799d961cebb0294fde76f68c08d27
Troyqwet <priveae@gmail.com>
2026-04-19 04:39:26 +0300

    Fix OpenNext config format

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,20 +1,3 @@
-import type { OpenNextConfig } from 'open-next/types/open-next'
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-const config: OpenNextConfig = {
-  default: {
-    override: {
-      wrapper: 'cloudflare-node',
-      converter: 'edge',
-    },
-  },
-  middleware: {
-    external: true,
-    override: {
-      wrapper: 'cloudflare-edge',
-      converter: 'edge',
-      proxyExternalRequest: 'fetch',
-    },
-  },
-}
-
-export default config
+export default defineCloudflareConfig({});
