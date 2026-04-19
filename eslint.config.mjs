f4369a9bbdb5ac5f3628440161ca3c60710dd688
Troyqwet <priveae@gmail.com>
2026-04-19 13:53:51 +0300

    Fix eslint flat config and stripe apiVersion for v17 compatibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  {
+    rules: {
+      "@next/next/no-html-link-for-pages": "warn",
+    },
+  },
   globalIgnores([
     ".next/**",
     "out/**",
